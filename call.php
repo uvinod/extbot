218aa9e30542a62cef39da9160ce1a17fd7f02c2
Vinod <vinodkumar.u93@gmail.com>
2016-10-20 21:39:54 +0800

    Update call.php

--- call.php
+++ call.php
@@ -1,36 +1,41 @@
 <?php
-function connect_db($db_host, $db_username, $db_password, $db_name){
-	// Create connection
-	$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
-	// Check connection
+function connect_db(){
+	$conn = new mysqli(getenv('MYSQL_DB_HOST') . ':' . getenv('MYSQL_DB_PORT'), getenv('MYSQL_DB_USERNAME'), getenv('MYSQL_DB_PASSWORD'), getenv('MYSQL_DB_NAME'));
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
 	return $conn;
 }
+function fetch_content($trigger_word, $text){
+	$conn = connect_db();
+	$keyword = rtrim(ltrim(str_replace($_POST["trigger_word"], "", $_POST["text"])));
+	$sql = "SELECT description from articles where keyword = '".$keyword."'";
+	
+	$result = $conn->query($sql);
+	$result_content = "Keyword doesn't match";
+	if ($result->num_rows > 0) {
+	    $row = $result->fetch_assoc();
+	    $result_content = $row["description"];
+	}
+	mysqli_close($conn);
+	return $result_content;
+}
+function post_content($username, $channel_name, $result_content){
+	$data = json_encode(array(        
+			"username"		=>	$username, 
+	        "channel"       =>  "#{$channel_name}",
+	        "text"          =>  $result_content
+	    ));
+	return $data;
+}
 
-define( "SLACK_TOKEN", getenv["SLACK_TOKEN"] );
+define( "SLACK_TOKEN", getenv("SLACK_TOKEN") );
 
 if($_POST["token"] != SLACK_TOKEN) {
 	$result_description = "Error: Invalid token";
 } else {
-	$conn = connect_db(getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), getenv('OPENSHIFT_APP_NAME'));
-	$keyword = rtrim(ltrim(str_replace($_POST["trigger_word"], "", $_POST["text"])));
-	$sql = "SELECT description from articles where keyword = '".$keyword."'";
-	$result = $conn->query($sql);
-	$result_description = "Keyword doesn't match";
-	if ($result->num_rows > 0) {
-	    $row = $result->fetch_assoc();
-	    $result_description = $row["description"];
-	}
-	mysqli_close($conn);
+	$result_content = fetch_content($_POST["trigger_word"], $_POST["text"]);
+	$response = post_content("Team Bot Tom", $_POST["channel_name"], $result_content);
+	echo $response;
 }
-$room = $_POST["channel_name"]; 
-$data = json_encode(array(        
-		"username"		=>	"Team Bot Tom", 
-        "channel"       =>  "#{$room}",
-        "text"          =>  $result_description
-    ));
-
-echo $data;
 ?>
